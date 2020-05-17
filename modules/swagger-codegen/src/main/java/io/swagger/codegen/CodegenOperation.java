15089820426d59c88ed3be191a2a41b6f2d985a8
Samuel Kahn <samuel@kahncode.com>
2020-05-17 23:25:09 +0200

    Cleanups and samples

--- modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenOperation.java
+++ modules/swagger-codegen/src/main/java/io/swagger/codegen/CodegenOperation.java
@@ -133,7 +133,7 @@ public class CodegenOperation {
     /**
      * Check if act as Restful show method
      *
-     * @return true if act as Restful show method, false otherwisec
+     * @return true if act as Restful show method, false otherwise
      */
     public boolean isRestfulShow() {
         return "GET".equalsIgnoreCase(httpMethod) && isMemberPath();
